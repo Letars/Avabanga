8ef5e07361ffe0621bb9b14c9e496bfb297cd963
Letars <e.sichevskaya@inbox.ru>
2021-07-18 13:45:59 +0300

    2 ДЗ

---
+++ src/Lesson02/HomeWork2.java
@@ -0,0 +1,58 @@
+package Lesson02;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+
+        System.out.println(method1(11, 1));
+        System.out.println(method1(11, 12));
+        System.out.println("__________________________");
+
+        method2(12);
+        method2(-9);
+        System.out.println("__________________________");
+
+        System.out.println(method3(92));
+        System.out.println(method3(-8));
+        System.out.println("__________________________");
+
+        method4("Test",4);
+    }
+
+/*    1. Написать метод, принимающий на вход два целых числа и проверяющий,
+что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.*/
+
+    public static boolean method1(int a, int b) {
+        return a + b >=10 && a + b <=20;
+    }
+
+/*
+    2. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль,
+    положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.*/
+
+    public static void method2(int a) {
+        if (a >=0){
+            System.out.println("Положительное число = " + a);
+        }
+        else {
+            System.out.println("Отрицательное число = " + a);
+        }
+    }
+
+    /*3. Написать метод, которому в качестве параметра передается целое число.
+    Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.*/
+
+    public static boolean method3(int a) {
+        return a  <=0;
+    }
+
+/*    4. Написать метод, которому в качестве аргументов передается строка и число,
+    метод должен отпечатать в консоль указанную строку, указанное количество раз;*/
+
+    public static void method4(String a, int b) {
+       int c = 0;
+        while (c < b){
+            c++;
+           System.out.println(a + c);
+        }
+    }
+}
