b772d88f2b1239361f44a0c2dd342b6e812db895
Letars <e.sichevskaya@inbox.ru>
2021-08-01 22:24:10 +0300

    ДЗ к 6 уроку

---
+++ src/Lesson06/Dog.java
@@ -0,0 +1,10 @@
+package Lesson06;
+
+import Lesson06.model.Animal;
+
+public class Dog extends Animal {
+
+    public Dog(String name, int run, int swim) {
+        super(name, run, swim);
+    }
+}
